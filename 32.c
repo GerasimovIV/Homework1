188676a10a4057af10fcf9c4a4b91a82c3206280
IvanOfficial <31989152+IvanOfficial@users.noreply.github.com>
2017-12-14 15:35:54 +0300

    Update 32.c
    after Issues

--- 32.c
+++ 32.c
@@ -1,40 +1,23 @@
-# Homework1
 #include <stdio.h>
 #include <stdlib.h>
-bool F(n)
+int F(int n)
 {
-    if (n == 1)
+    if ((n == 1) || (n == 2))
     {
-        return false;
+        return 1;
     }
-    if (n == 0)
-    {
-        return false;
-    }
-    for (i=2; i<= n/2; i++)
-    {
-        d = n/i;
-        if (d*i == n)
-        {
-            return false;
-        }
-    }
-    return true;
-
+    return F(n-1)+F(n-2);
 }
-int main()
+
+int main(void)
 {
     int n;
-    scanf("%d", n);
-    bool h;
-    h = F(n);
-    if (h ==false)
+    scanf("%d", &n);
+    int i = 1;
+    /*for (i = 1; i <= n; i ++)
     {
-        printf("Не простое");
-    }
-    if (h == true)
-    {
-        printf("Простое");
-    }
+        printf("%d\n", F(i));
+    }*/
+    printf("%d\n", F(n));
     return 0;
 }
